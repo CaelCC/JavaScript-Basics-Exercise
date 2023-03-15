3a2e66a0bdd544072064cf11fb22763ba0df6e9b
Cael Clarkson <caelclarkson@gmail.com>
2023-03-15 09:28:16 -0500

    finished excercise

---
+++ script.js
@@ -0,0 +1,50 @@
+// 1
+console.log (42);
+
+// 2
+// String, Number, Boolean, Null, Undefined
+
+// 3
+// +,-,*,/,**,%
+
+// 4
+console.log(5 % 2);
+console.log(5 %4);
+
+// 5
+console.log ('I\'M MARRY POPPINS Y\'ALL');
+
+// 6
+console.log("Hawk" + "eye");
+
+// 7
+console.log("Cael".length + "Clarkson".length);
+
+// 8
+"Avenger: Endgame"[16];
+
+// Bonus
+// 1
+console.log("\\");
+
+// 2
+console.log(5 ** 4 - 9);
+
+// 3
+console.log((("I".length + "AM".length) % "IRON".length) % "MAN".length) // 0
+
+// 4
+console.log("CAP"[1]); 
+console.log("VISION"[0]); 
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT")[0];
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5
+let m = 25;
+let c = 4;
+let u = m + c
+console.log(u);
